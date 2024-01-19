5fd378edd989b7c12bcc030ca3f27fb78df3b827
Ayush Patange <patangeayush998@gmail.com>
2024-01-19 19:17:18 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,6 +1,10 @@
 package shapes_area_calculator;
 
+// This is a child class named as Rectangle. It has abstract method inherited from Class Shapes.
+
 public class Rectangle extends Shapes{
+
+	// Method to calculate Area of rectangle. Its take lenght and breadth as parameter.
 	
 	double printArea(double lenght, double breadth) {
 		double area= lenght*breadth;
