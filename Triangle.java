1a9b3d1d80981e0ae7bca8dc21e6cd8eaecb9c28
Ayush Patange <patangeayush998@gmail.com>
2024-01-19 19:23:06 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,7 +1,11 @@
 package shapes_area_calculator;
 
+// This is a child class named as Triangle has abstract method inherited from Class Shapes.
+
 public class Triangle extends Shapes {
 
+	// Method to calculate area of triangle . It take two parameter lenght and breadth.
+	
 	double printArea(double lenght, double breadth) {
 		double area= (lenght*breadth)/2;
 		return area;
