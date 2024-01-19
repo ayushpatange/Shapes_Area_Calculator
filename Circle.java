11fbb1a83691c036835657677bb7c7ac939f964d
Ayush Patange <patangeayush998@gmail.com>
2024-01-19 19:09:27 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,9 +1,11 @@
 package shapes_area_calculator;
 
+// This is a child class named as circle. It has abstract method inherited from Class Shapes.
 public class Circle extends Shapes  {
 	
 	static double pie= 3.14;
 	double radius;
+	// This method calculate Area of a circle by taking radius as parameter.
 	double printArea( double radius) {
 		double area= pie*(radius*radius);
 		return area;
